f5f59e40e07b67080d0b91dea0872f6bb7445eab
Uhmpasterig <805luca805@gmail.com>
2024-03-23 21:15:42 +0100

    fix: installed the neweset version of config service

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,21 +5,5 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+  extends: ['@socialize-app/eslint-config/config/nest.js'],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService, ConfigModule } from '@socialize-app/config';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+
+type Config = {
+  port: number;
+};
+
+const retrieveConfig = async (): Promise<Config> => {
+  const configModule = await NestFactory.create(ConfigModule.register());
+  const configService = configModule.get(ConfigService);
+  const port = await configService.get('USER_SERVICE_PORT');
+
+  console.log('port:', port);
+
+  configModule.close();
+
+  return {
+    port: parseInt(port),
+  };
+};
 
 async function bootstrap() {
+  const { port } = await retrieveConfig();
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.TCP,
       options: {
-        port: 3001,
+        port,
       },
     },
   );
